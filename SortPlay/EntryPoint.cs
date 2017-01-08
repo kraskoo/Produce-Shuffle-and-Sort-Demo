c959171f9526abde87c61de499eb2fb3fc03f030
kraskoo <kraskoo87@gmail.com>
2017-01-08 22:31:37 +0200

    Fix arrays length to be more reflected efficiency

--- SortPlay/EntryPoint.cs
+++ SortPlay/EntryPoint.cs
@@ -11,44 +11,44 @@
         public static void Main()
         {
             DefaultResults();
-            Console.WriteLine("50 Items");
+            Console.WriteLine("500 Items");
             Console.WriteLine("----------------");
             ResultWithArrayOf50Items();
-            Console.WriteLine($"{Environment.NewLine}100 Items");
+            Console.WriteLine($"{Environment.NewLine}1000 Items");
             Console.WriteLine("----------------");
             ResultWithArrayOf100Items();
-            Console.WriteLine($"{Environment.NewLine}150 Items");
+            Console.WriteLine($"{Environment.NewLine}1500 Items");
             Console.WriteLine("----------------");
             ResultWithArrayOf150Items();
-            Console.WriteLine($"{Environment.NewLine}200 Items");
+            Console.WriteLine($"{Environment.NewLine}2000 Items");
             Console.WriteLine("----------------");
             ResultWithArrayOf200Items();
         }
 
         private static void ResultWithArrayOf200Items(bool toShowItems = false)
         {
-            var items = Enumerable.Range(1, 200).ToArray();
+            var items = Enumerable.Range(1, 2000).ToArray();
             Shuffle.ShuffleItems(items);
             items.Print(toShowItems);
         }
 
         private static void ResultWithArrayOf150Items(bool toShowItems = false)
         {
-            var items = Enumerable.Range(1, 150).ToArray();
+            var items = Enumerable.Range(1, 1500).ToArray();
             Shuffle.ShuffleItems(items);
             items.Print(toShowItems);
         }
 
         private static void ResultWithArrayOf100Items(bool toShowItems = false)
         {
-            var items = Enumerable.Range(1, 100).ToArray();
+            var items = Enumerable.Range(1, 1000).ToArray();
             Shuffle.ShuffleItems(items);
             items.Print(toShowItems);
         }
 
         private static void ResultWithArrayOf50Items(bool toShowItems = false)
         {
-            var items = Enumerable.Range(1, 50).ToArray();
+            var items = Enumerable.Range(1, 500).ToArray();
             Shuffle.ShuffleItems(items);
             items.Print(toShowItems);
         }
